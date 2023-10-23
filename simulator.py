26b11c6088697e661cfbe12179c06186f6dab9c2
Dhanvant <92245578+Dhanvantg@users.noreply.github.com>
2023-10-23 23:08:08 +0530

    Deleted useless code

--- simulator.py
+++ simulator.py
@@ -14,7 +14,7 @@ pygame.mouse.set_visible(False)
 
 WIDTH, HEIGHT = WIN.get_size()
 
-# define some commonly used colours
+#commonly used colours
 WHITE = (255, 255, 255)
 LIGHTGREY = (192, 192, 192)
 DARKGREY = (128, 128, 128)
@@ -293,23 +293,6 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10 ** 30)
-    sun.sun = True
-
-    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10 ** 24)
-    earth.y_vel = 29.783 * 1000
-
-    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10 ** 23)
-    mars.y_vel = 24.077 * 1000
-
-    mercury = Planet(0.387 * Planet.AU, 0, 8, LIGHT_GREY, 3.30 * 10 ** 23)
-    mercury.y_vel = -47.4 * 1000
-
-    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10 ** 24)
-    venus.y_vel = -35.02 * 1000
-
-    #planets = [sun, earth, mars, mercury, venus]
-
     planets = []
     bl = []
     l = []
@@ -321,6 +304,7 @@ def main():
     first = True
     menu = True
     load = False
+          
     global lines
     lines = True
     speed = 1
